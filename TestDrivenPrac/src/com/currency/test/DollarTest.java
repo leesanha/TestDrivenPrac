75c064864d6db341b2edcec1aa1c981555c7118c
sana <bluegisa@daum.net>
2019-12-13 10:46:07 +0900

    복사 붙여 넣기를 통해서 Franc 클래스를 만듦

--- TestDrivenPrac/src/com/currency/test/DollarTest.java
+++ TestDrivenPrac/src/com/currency/test/DollarTest.java
@@ -1,6 +1,8 @@
 package com.currency.test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +13,13 @@ class DollarTest {
 		Dollar five = new Dollar(5);
 		assertEquals(new Dollar(10), five.times(2));
 		assertEquals(new Dollar(15), five.times(3));
+	}
+
+	@Test
+	void testFrancMultiplication() {
+		Franc five = new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));
 	}
 
 	@Test
