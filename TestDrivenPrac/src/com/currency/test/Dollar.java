453836421edaebdcdc0064173a404e7fdc221f68
sana <bluegisa@daum.net>
2019-12-13 10:22:47 +0900

    Equals 수정

--- TestDrivenPrac/src/com/currency/test/Dollar.java
+++ TestDrivenPrac/src/com/currency/test/Dollar.java
@@ -2,12 +2,17 @@ package com.currency.test;
 
 public class Dollar {
 	public int amount;
-	
+
 	public Dollar(int amount) {
 		this.amount = amount;
 	}
-	
+
 	public Dollar times(int multiplier) {
 		return new Dollar(amount * multiplier);
+	}
+
+	public boolean equals(Object object) {
+		Dollar dollar = (Dollar) object;
+		return amount == dollar.amount;
 	}
 }
